f6633576f8d8b590d3c8497d81e0a7628c298edb
veetaw <veetaw@users.noreply.github.com>
2017-09-10 19:26:29 +0200

    search agency

--- llpy/agency/__init__.py
+++ llpy/agency/__init__.py
@@ -1,5 +1,6 @@
-from llpy.constants import URL
 import requests
+
+from llpy.constants import URL
 
 
 class Agency:
@@ -17,21 +18,29 @@ class Agency:
             self.__parse()
 
         else:
-            # TODO: return a dict with all agencies
-            pass
+            try:
+                self.agencies = requests.get(URL + 'agency').json()['agencies']
+            except:
+                raise Exception('error')
 
     def __parse(self):
-        r = requests.get(URL + 'agency/' + self._agency).json()['agencies'][0]
-        if r:
-            try:
-                self.agency_id = r['id']
-                self.agency_name = r['name']
-                self.agency_countryCode = r['countryCode']
-                self.agency_abbrev = r['abbrev']
-                self.agency_type = r['type']
-                self.agency_wikiURL = r['wikiURL']
-                self.agency_infoURLs = r['infoURLs'] if r['infoURLs'] else None
-            except:
-                raise Exception
-        else:
+        try:
+            r = requests.get(URL + 'agency/' + self._agency).json()['agencies'][0]
+        except:
+            raise Exception('error in request')
+        try:
+            self.agency_id = r['id']
+            self.agency_name = r['name']
+            self.agency_countryCode = r['countryCode']
+            self.agency_abbrev = r['abbrev']
+            self.agency_type = r['type']
+            self.agency_wikiURL = r['wikiURL']
+            self.agency_infoURLs = r['infoURLs'] if r['infoURLs'] else None
+        except:
             raise Exception
+
+    def search(self, query):
+        try:
+            return requests.get(URL + 'agency?name=' + query).json()['agencies']
+        except:
+            raise Exception('error')
