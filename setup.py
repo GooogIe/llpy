29cdee11d3659173411af1e061b5d41e9d0a9431
veetaw <veetaw@users.noreply.github.com>
2017-09-10 19:08:13 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='llpy',
+    version='0.0.1',
+    packages=['llpy'],
+    url='https://github.com/veetaw/llpy',
+    license='GNU General Public License v3.0',
+    author='veetaw',
+    author_email='',
+    description=''
+)
