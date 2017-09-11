1f6e0aa004e7bcfa609f646c43fad5769692aa87
veetaw <veetaw@users.noreply.github.com>
2017-09-11 09:48:00 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='llpy',
-    version='0.0.1',
+    version='0.0.2',
     packages=['llpy'],
     url='https://github.com/veetaw/llpy',
     license='GNU General Public License v3.0',
