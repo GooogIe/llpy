406159fd96e9466044076d4c5e2eb1d16b6e3589
qiqvo <xanderprix@gmail.com>
2017-09-13 23:57:32 +0300

    query  is more proper name for the variable

--- llpy/agency/__init__.py
+++ llpy/agency/__init__.py
@@ -5,19 +5,19 @@ from llpy.constants import URL
 
 
 class Agency(Base):
-    def __get_the(self, agency):
-        Base.__get_the(self, ['agency', agency, 'agencies'])
+	def __get_the(self, query):
+		Base.__get_the(self, ['agency', query, 'agencies'])
 
-    def __parse(self, raw):
-        Base.__parse(self, raw)
+	def __parse(self, raw):
+		Base.__parse(self, raw)
 
-    def search(self, query):
-        try:
-            return requests.get(URL + 'agency?name=' + query).json()['agencies']
-        except Exception as e:
-            raise LLpyException('search', str(e))
+	def search(self, query):
+		try:
+			return requests.get(URL + 'agency?name=' + query).json()['agencies']
+		except Exception as e:
+			raise LLpyException('search', str(e))
 
 
 class AgencyType(Base):
-    def __get_the(self, agencytype):
-        Base.__get_the(self, ['agencytype', agencytype, 'types'])
+	def __get_the(self, query):
+		Base.__get_the(self, ['agencytype', query, 'types'])
