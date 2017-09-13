8c822d275843a23f514bcaf0684d95d0ed242443
qiqvo <xanderprix@gmail.com>
2017-09-13 23:58:04 +0300

    Rocket implimented from Base

---
+++ llpy/rocket/__init__.py
@@ -0,0 +1,24 @@
+import requests
+
+from llpy import LLpyException, Base
+from llpy.constants import URL
+
+
+class Rocket(Base):
+	def __get_the(self, query):
+		Base.__get_the(self, ['rocket', query, 'rockets'])
+
+	def search(self, query):
+		try:
+			return requests.get(URL + 'agency?name=' + query).json()['agencies']
+		except Exception as e:
+			raise LLpyException('search', str(e))
+
+
+class RocketEvent(Base):
+	def __get_the(self, query):
+		Base.__get_the(self, ['rocketevent', query, ''])
+
+class RocketFamily(Base):
+	def __get_the(self, query):
+		Base.__get_the(self, ['rocketfamily', query, 'RocketFamilies'])
