8e79af2dd07891794be1cb76eb2c46c82445d06d
veetaw <veetaw@users.noreply.github.com>
2017-09-14 17:28:38 +0200

    tabs are never good

--- llpy/agency/__init__.py
+++ llpy/agency/__init__.py
@@ -5,19 +5,19 @@ from llpy.constants import URL
 
 
 class Agency(Base):
-	def __get_the(self, query):
-		Base.__get_the(self, ['agency', query, 'agencies'])
+    def __get_the(self, query):
+        Base.__get_the(self, ['agency', query, 'agencies'])
 
-	def __parse(self, raw):
-		Base.__parse(self, raw)
+    def __parse(self, raw):
+        Base.__parse(self, raw)
 
-	def search(self, query):
-		try:
-			return requests.get(URL + 'agency?name=' + query).json()['agencies']
-		except Exception as e:
-			raise LLpyException('search', str(e))
+    def search(self, query):
+        try:
+            return requests.get(URL + 'agency?name=' + query).json()['agencies']
+        except Exception as e:
+            raise LLpyException('search', str(e))
 
 
 class AgencyType(Base):
-	def __get_the(self, query):
-		Base.__get_the(self, ['agencytype', query, 'types'])
+    def __get_the(self, query):
+        Base.__get_the(self, ['agencytype', query, 'types'])
--- llpy/rocket/__init__.py
+++ llpy/rocket/__init__.py
@@ -5,20 +5,21 @@ from llpy.constants import URL
 
 
 class Rocket(Base):
-	def __get_the(self, query):
-		Base.__get_the(self, ['rocket', query, 'rockets'])
+    def __get_the(self, query):
+        Base.__get_the(self, ['rocket', query, 'rockets'])
 
-	def search(self, query):
-		try:
-			return requests.get(URL + 'agency?name=' + query).json()['agencies']
-		except Exception as e:
-			raise LLpyException('search', str(e))
+    def search(self, query):
+        try:
+            return requests.get(URL + 'agency?name=' + query).json()['agencies']
+        except Exception as e:
+            raise LLpyException('search', str(e))
 
 
 class RocketEvent(Base):
-	def __get_the(self, query):
-		Base.__get_the(self, ['rocketevent', query, ''])
+    def __get_the(self, query):
+        Base.__get_the(self, ['rocketevent', query, ''])
+
 
 class RocketFamily(Base):
-	def __get_the(self, query):
-		Base.__get_the(self, ['rocketfamily', query, 'RocketFamilies'])
+    def __get_the(self, query):
+        Base.__get_the(self, ['rocketfamily', query, 'RocketFamilies'])
